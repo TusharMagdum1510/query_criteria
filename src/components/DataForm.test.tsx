d6b5c064fa6be0dead8a956bb40605ecf8584563
TusharMagdum1510 <149376030+TusharMagdum1510@users.noreply.github.com>
2024-08-08 15:51:46 +0530

    Created test cases for different query criteria

--- src/components/DataForm.test.tsx
+++ src/components/DataForm.test.tsx
@@ -4,7 +4,19 @@ import DataForm from "./DataForm";
 
 test("Selecting different elements", () => {
   render(<DataForm />);
-  
-  const button = screen.getByRole('button');
-  expect(button).toBeInTheDocument();
+
+  const elements = [
+    screen.getByRole("button"),
+    screen.getByLabelText("Email"),
+    screen.getByPlaceholderText("Red"),
+    screen.getByText("Enter Data"),
+    screen.getByDisplayValue("email@gmail.com"),
+    screen.getByAltText("data"),
+    screen.getByTitle(/Click when ready to submit/i),
+    screen.getByTestId("image wrapper"),
+  ];
+
+  for (const element of elements) {
+    expect(element).toBeInTheDocument();
+  }
 });
